17231f7a0bd31e6f96dcf4c134bce3f9e915e656
Dimitriy159 <dmshiryakin@gmail.com>
2017-12-07 01:08:44 +0200

    qwer

--- questions.js
+++ questions.js
@@ -51,3 +51,54 @@ ipod.volumePlus();
 console.log(ipod.getVolume());
 ipod.volume(-19);
 console.log(ipod.getVolume());
+
+/*Напишите конструктор для объектов Сумматор, со следующим функционалом:
+1. Приватное свойство firstNumber;
+2. Приватное свойство secondNumber;
+3. Публичное свойство result;
+4. Приватный метод calc(), который записывает в result сумму свойств
+firstNumber и secondNumber;
+5. Единые методы геттер-сеттер для свойств firstNumber и secondNumber, во
+время работы методов как сеттер, должна происходить валидация (устанавливать
+можно только значения типа Number), если валидация происходит успешно, должен
+вызываться метод calc(). Получается, что при успешной установке либо firstNumber
+либо secondNumber, происходит перерасчет result.*/
+"use strict";
+function Sum(firstNumber, secondNumber){
+	var _firstNumber=0;
+	var _secondNumber=0;
+	this.result=0;
+	function culc(){
+		this.result = firstNumber + secondNumber;
+	};
+	culc();
+	function valid1(firstNumber){
+		if(typeof(firstNumber)==="number"){
+			_firstNumber=firstNumber;
+			culc();
+		}
+		else console.log("Enter the correct value");
+	};
+	function valid2(secondNumber){
+		if(typeof(secondNumber)==="number"){
+			_secondNumber=secondNumber;
+			culc();
+		}
+		else console.log("Enter the correct value");
+	};
+	this.firstNumber = function(firstNumber){
+		if (firstNumber === undefined) 
+			return _firstNumber;
+		valid1(firstNumber);
+	};
+	this.secondNumber = function(secondNumber){
+		if (secondNumber === undefined) 
+			return _secondNumber;
+		valid2(secondNumber);
+	};
+}
+var sum = new Sum(23, 17);
+sum.culc();
+sum.firstNumber(17);
+sum.secondNumber(13);
+console.log(sum.result);
